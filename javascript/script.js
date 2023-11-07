a3dac76e81b2f562017f0f6bd40a33a565c93b90
Tone Alsvik Finstad <toneaf@online.no>
2023-11-07 10:01:23 +0100

    Added pictures and changed activities and javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,11 @@
 
-type="text/javascript">
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+  
+function googleTranslate() {
+  new google.translate.TranslateElement({pageLanguage: 'no', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate');
 }
 
+document.getElementById('translateButton').addEventListener('click', function() {
+  googleTranslate();
+});
 
+/* https://www.w3schools.com/howto/howto_google_translate.asp */
