1111a542a2f802cd9d3d8ee5f266fade1a35d96a
Hallvard <hallvardfw@hotmail.no>
2023-11-07 13:27:57 +0100

    header

--- javascript/header_footer.js
+++ javascript/header_footer.js
@@ -1,18 +1,20 @@
 
 const header = document.querySelector('header');
 header.innerHTML = `
-<div class="header_footer">
+
+<div class="header">
    
     <a href="#html">Transportation</a>
+    <img src="img/Hallvard.PNG" alt="" class="bilde"> 
     <a href="#html">Contact</a>
     <a href="#html">Activites</a>
 </div>
-   
+
 `;
 
 const footer = document.querySelector('footer');
 footer.innerHTML = `
-<div class="header_footer">
+<div class="footer">
     This is a project about this and that   
 </div>
 `;
