165cddb0e79d6154d3136fb31bbb88cf2c8d5eca
hannafalch <hannafn99@hotmail.com>
2023-11-08 14:16:29 +0100

    Added nav bar

---
+++ javascript/dropdown.js
@@ -0,0 +1,66 @@
+
+
+const header = document.querySelector('header');
+header.innerHTML = `
+
+<div class="header">
+<nav id=navbar>
+    <ul>
+    <li><a href="#">Aktiviteter</a>
+        <ul>
+            <li><a href="#">NTNUI</a></li>
+            <li><a href="#">Turmuligheter</a></li>
+            <li><a href="#">Sit Trening</a></li>
+        </ul>
+    </li>
+    <li><a href="#">Geografi</a>
+        <ul>
+        <li><a href="#">Kart</a></li>  
+        <li><a href="#">Transport</a></li>
+        </ul>
+    </li>
+    <li>          </li>
+    <li>          </li>
+    <li>          </li>
+    <li>          </li>
+    <li>          </li>
+    <li>          </li>
+    <li>          </li>
+    <li><a href="#">Tradisjoner</a>
+        <ul>
+            <li><a href="#">Studentviser</a></li>
+            <li><a href="#">Hva er dette?</a></li>
+            <li><a href="#">Hva betyr det?</a></li>
+            <li><a href="#">Fadderuka</a></li>
+            <li><a href="#">Utesteder</a></li>
+        </ul>
+        </li> 
+    <li><a href="#">Frivillige verv</a>
+        <ul>
+        <li><a href="#">Linjeforeninger</a></li>
+        <li><a href="#">Uka</a></li>
+        <li><a href="#">Samfundet</a></li>
+        <li><a href="#">Faglige verv</a></li>
+        </ul>
+    </li>
+    </ul>
+</nav>
+</div>
+<div class="logo">
+    <img src="img/Logo.png" alt="Logo inkluderingsbanden" style="width: 250px">
+</div>
+
+`;
+
+
+
+
+
+const footer = document.querySelector('footer');
+footer.innerHTML = `
+<div class="footer">
+    <p>Inkluderingsbanden</p>
+</div>
+`;
+const headerOffset = header.offsetTop; 
+const footerOffset = footer.offsetTop;
